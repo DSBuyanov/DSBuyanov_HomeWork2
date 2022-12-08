2d4f73c5ae785e05bab1bc5a8e40454d6c34f043
Denis <inquis87@gmail.com>
2022-12-08 16:53:52 +0300

    3

--- task3/Program.cs
+++ task3/Program.cs
@@ -5,35 +5,15 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
-// Console.Write("Введите число: ");
-// int cube = Convert.ToInt32(Console.ReadLine());
-
-// void Cube(int[] cube)
-// {
-//     int counter = 0;
-//     int length = cube.Length;
-//     while (counter < length)
-//     {
-//         cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
-//         counter++;
-//     }
-// }
-
-// void PrintArry(int[] coll)
-// {
-//     int count = coll.Length;
-//     int index = 0;
-//     while (index < count)
-//     {
-//         Console.Write(coll[index] + " ");
-//         index++;
-//     }
-// }
-
-// int[] arry = new int[cube + 1];
-// Cube(arry);
-// PrintArry(arry);
 
 Console.WriteLine("Введите число: ");
 int N = int.Parse(Console.ReadLine()!);
+int num = 1;
+// Console.WriteLine($"Таблица кубов числа: {N}, от 1 до числа {N}");
+while (num < N + 1)
+{
+    int result = num * num * num;
+    Console.Write(result + ",");
+    num++;
+}
 
